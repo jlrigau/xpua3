f709b1df84db5945107553cc79c057a8ae160bff
Morgan Renou <mrenou@gmail.com>
2013-02-06 15:08:55 +0100

    add multiple concert

--- pillar3-web/src/main/java/fr/xebia/pillar3/web/resource/ConcertResource.java
+++ pillar3-web/src/main/java/fr/xebia/pillar3/web/resource/ConcertResource.java
@@ -1,5 +1,6 @@
 package fr.xebia.pillar3.web.resource;
 
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -12,6 +13,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 @Singleton
@@ -19,6 +21,7 @@ import java.util.Random;
 public class ConcertResource {
 
     UserRepository userRepository;
+    private Random random = new Random();
 
     @Inject
     public ConcertResource(UserRepository userRepository) {
@@ -29,11 +32,25 @@ public class ConcertResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{artistName}")
     public Response findConcerts(@CookieParam("login") String login, @PathParam("name") String artistName) {
-        Date date = generateDateInto2NextWeeks().toDate();
-        User user = userRepository.findByLogin(login);
-        double latitude = user.latitude + (Math.random() / 2);
-        double longitude = user.longitude + (Math.random() / 2);
-        return Response.ok(new Gson().toJson(new Concert(artistName, date, longitude, latitude))).build();
+        List<Concert> concerts = Lists.newArrayList();
+        for (int i = 0; i < random.nextInt() % 15; i++) {
+            Date date = generateDateInto2NextWeeks().toDate();
+            User user = userRepository.findByLogin(login);
+            double latitude;
+            if (random.nextBoolean()) {
+                latitude = user.latitude + (Math.random() / 2);
+            } else {
+                latitude = user.latitude - (Math.random() / 2);
+            }
+            double longitude;
+            if (random.nextBoolean()) {
+                longitude = user.longitude + (Math.random() / 2);
+            } else {
+                longitude = user.longitude - (Math.random() / 2);
+            }
+            concerts.add(new Concert(artistName, date, longitude, latitude));
+        }
+        return Response.ok(new Gson().toJson(concerts)).build();
     }
 
     private DateTime generateDateInto2NextWeeks() {
