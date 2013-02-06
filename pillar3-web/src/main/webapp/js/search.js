eeeb6a51a9bb4deb370f4e3fdae810d83847b13c
Julien Buret <julien.buret@gmail.com>
2013-02-06 14:32:50 +0100

    navigate into similarity

--- pillar3-web/src/main/webapp/js/search.js
+++ pillar3-web/src/main/webapp/js/search.js
@@ -9,17 +9,17 @@ define(['jquery', 'bootstrap'], function ($, bootstrap) {
 
             source: function (search, typeahead) {
                 /*var artist = {
-                    query: {
-                        multi_match: {
-                            query: search.toLowerCase(),
-                            fields: [
-                                'name^4',
-                                'tracks.title^2',
-                                'tracks.release'
-                            ]
-                        }
-                    }
-                }*/
+                 query: {
+                 multi_match: {
+                 query: search.toLowerCase(),
+                 fields: [
+                 'name^4',
+                 'tracks.title^2',
+                 'tracks.release'
+                 ]
+                 }
+                 }
+                 }*/
                 var artist = {
                     'query': {
                         'fuzzy': {
@@ -29,7 +29,7 @@ define(['jquery', 'bootstrap'], function ($, bootstrap) {
                 }
 
 
-                return $.post('http://ec2-54-246-72-133.eu-west-1.compute.amazonaws.com:9200/_search', JSON.stringify(artist), function (data) {
+                return $.post('http://ec2-54-246-72-133.eu-west-1.compute.amazonaws.com:9200/_search/', JSON.stringify(artist), function (data) {
                     var i = 0,
                         terms = [];
                     for (i = 0; i < data.hits.hits.length; i++) {
@@ -44,61 +44,66 @@ define(['jquery', 'bootstrap'], function ($, bootstrap) {
             },
 
             updater: function (item) {
-                var artist = artists[item];
-                $('#artists').show();
-                $.ajax({
-                    url: 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=small&imgtype=face&q=' + artist.name,
-                    success: function (data) {
-                        $('#artist_photo').attr('src', data.responseData.results[0].url);
-                    },
-                    crossDomain: true,
-                    type: 'GET',
-                    dataType: 'jsonp'
 
-                });
-
-                $('#artist_name').text(artist.name);
-                $('#artist_style').text(artist.mbid);
-                var albums = {}
-                $.each(artist.tracks, function () {
-                    if (albums[this.release] == undefined) {
-                        albums[this.release] = this.release
-                    }
-                })
-                var als = ''
-                $.each(albums, function () {
-                    als = als + this;
-                    als = als + ' ';
-
-                });
-                $('#artists_album').text(als);
-                $('#artists_similaire').text(artist.similarity);
-
-                $('#typeahead').val(item);
+                up(item)
             }
 
 
         });
 
 
-        this.searchArtist = function (id) {
+        var up = function (item) {
+            $('#typeahead').val(item);
 
-            var artist = {
-                query: {
-                    match: {
-                        artist_id: id
+            var artist = artists[item];
+            $('#artists').show();
+            $.ajax({
+                url: 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=small&imgtype=face&q=' + artist.name,
+                success: function (data) {
+                    $('#artist_photo').attr('src', data.responseData.results[0].url);
+                },
+                crossDomain: true,
+                type: 'GET',
+                dataType: 'jsonp'
+
+            });
+
+            $('#artist_name').text(artist.name);
+            $('#artist_id').text(artist.artist_id);
+            $('#similarity_id').text(artist.similarity_id);
+            $('#artist_style').text(artist.mbtag);
+            var albums = {}
+            $.each(artist.tracks, function () {
+                if (albums[this.release] == undefined) {
+                    albums[this.release] = this.release
+                }
+            })
+            var als = '<ul>'
+
+            $.each(albums, function () {
+                als = als + '<li>'
+                als = als + this;
+                als = als + '</li>'
+
+            });
+            als = als + '</ul>'
+            $('#artists_album').html(als);
+            $('#artists_similaire').html('<a href="" onclick="return false" class="simlink">' + artist.similarity + '</a>');
+            $('.simlink').click(function () {
+                var artistreq = {
+                    'query': {
+                        'match': {
+                            'artist_id': $('#similarity_id').text()
+                        }
                     }
                 }
-            }
-
-            $.post('http://ec2-54-246-72-133.eu-west-1.compute.amazonaws.com:9200/_search', JSON.stringify(artist), function (data) {
-                $('#artist-favorites').append('<li><a href="#artists" id="' + data[i].id + '" class="artist"><i class="icon-star"></i>' + data[i].name +'</a></li>');
+                $.post('http://ec2-54-246-72-133.eu-west-1.compute.amazonaws.com:9200/_search/', JSON.stringify(artistreq), function (data2) {
+                    var artist = data2.hits.hits[0]._source;
+                    artists[artist.name] = artist;
+                    up(artist.name)
+                })
             });
         }
-
-
-
     };
 
-})
-;
+});
