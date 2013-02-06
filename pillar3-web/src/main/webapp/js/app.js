ac17ab70a1d2403227cc4c23aed8e658019a7e8e
Gerome Egron <legunda.gg&gmail.com>
2013-02-06 16:26:17 +0100

    Change icon

--- pillar3-web/src/main/webapp/js/app.js
+++ pillar3-web/src/main/webapp/js/app.js
@@ -1,6 +1,6 @@
 require.config({
     baseUrl: 'js',
-
+    
     paths: {
         'jquery': [
             'http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min',
@@ -46,11 +46,11 @@ require.config({
         'bootstrap': ['jquery']
 
     },
-
+    
     waitSeconds: 15
 });
 
-require(['jquery', 'cookie', 'maps', 'search'], function ($, cookien, maps, search) {
+require(['jquery', 'cookie', 'maps', 'search'], function($, cookien, maps, search) {
     var COOKIE_NAME = "XPUA_3";
 
     var COOKIE_OPTIONS = {
@@ -95,7 +95,7 @@ require(['jquery', 'cookie', 'maps', 'search'], function ($, cookien, maps, sear
 
         if (user.badges) {
             for (var i = 0; i < user.badges.length; i++) {
-                $badges.append('<li><a href="#badges"><i class="icon-star"></i>' + user.badges[i] +'</a></li>');
+                $badges.append('<li><a href="#badges"><i class="icon-tag"></i>' + user.badges[i] +'</a></li>');
             }
         }
 
@@ -115,7 +115,7 @@ require(['jquery', 'cookie', 'maps', 'search'], function ($, cookien, maps, sear
         });
     }
 
-    function configureLogoutButton() {
+    function configureLogoutButton () {
         $('#artists-list').hide();
         $('#badges-list').hide();
 
@@ -126,7 +126,7 @@ require(['jquery', 'cookie', 'maps', 'search'], function ($, cookien, maps, sear
 
         $('#loginModalBtn').text('Se connecter');
         $('#loginModalBtn').unbind('click');
-        $('#loginModalBtn').click(function () {
+        $('#loginModalBtn').click(function(){
             $('#loginModal').modal();
         });
     }
@@ -145,7 +145,7 @@ require(['jquery', 'cookie', 'maps', 'search'], function ($, cookien, maps, sear
         var $email = $('#email').val(),
             $city = $('#city').val();
 
-        $.post('/resources/users/login', {login: $email, city: $city}, function (data) {
+        $.post('/resources/users/login', {login: $email, city:$city}, function (data) {
             console.log(data);
             var user = JSON.stringify(data);
             $.cookie(COOKIE_NAME, user, COOKIE_OPTIONS);
@@ -168,30 +168,4 @@ require(['jquery', 'cookie', 'maps', 'search'], function ($, cookien, maps, sear
 
     });
 
-    var lastUpdated = 0;
-    var displayOneNotif = function (notif) {
-        $("#notifications").prepend(
-            $('<li></li>').append(
-                notif.login +
-                    " a ajouté l'artiste " +
-                    notif.artist +
-                    " à ses favoris."
-            )
-        );
-    };
-
-    var getNotifications = function () {
-        $.ajax({
-            url: "/resources/notifications?since=" + lastUpdated
-        }).done(function (json) {
-                json.forEach(displayOneNotif);
-                // TODO purger les vieilles notifs
-
-                // timezone !!!
-                lastUpdated = new Date().getTime() + 6 * 3600 * 1000;
-
-                setTimeout(getNotifications, 5000);
-            });
-    };
-    getNotifications();
 });
