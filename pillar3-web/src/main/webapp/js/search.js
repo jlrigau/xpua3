edfafadbca8e9c32e1d4e6d1f76b0b9f011b39dc
Julien Buret <julien.buret@gmail.com>
2013-02-06 13:28:13 +0100

    fiche artist

--- pillar3-web/src/main/webapp/js/search.js
+++ pillar3-web/src/main/webapp/js/search.js
@@ -2,16 +2,16 @@ define(['jquery', 'bootstrap'], function ($, bootstrap) {
 
     return new function () {
 
-        var artists = [];
+        var artists = {};
 
         $('#typeahead').typeahead({
             items: 10,
 
             source: function (search, typeahead) {
-                var artist = {
+                /*var artist = {
                     query: {
-                    multi_match: {
-                        query: search.toLowerCase(),
+                        multi_match: {
+                            query: search.toLowerCase(),
                             fields: [
                                 'name^4',
                                 'tracks.title^2',
@@ -19,16 +19,22 @@ define(['jquery', 'bootstrap'], function ($, bootstrap) {
                             ]
                         }
                     }
+                }*/
+                var artist = {
+                    'query': {
+                        'fuzzy': {
+                            'name': search.toLowerCase()
+                        }
+                    }
                 }
 
 
-
-                return $.get('http://ec2-54-246-72-133.eu-west-1.compute.amazonaws.com:9200/_search', artist, function (data) {
+                return $.post('http://ec2-54-246-72-133.eu-west-1.compute.amazonaws.com:9200/_search', JSON.stringify(artist), function (data) {
                     var i = 0,
                         terms = [];
                     for (i = 0; i < data.hits.hits.length; i++) {
                         var artist = data.hits.hits[i]._source;
-                        artists.push(artist);
+                        artists[artist.name] = artist;
                         terms.push(artist.name);
                     }
 
@@ -39,11 +45,42 @@ define(['jquery', 'bootstrap'], function ($, bootstrap) {
 
             updater: function (item) {
                 $('#typeahead').val(item);
-                console.log(item);
+                var artist = artists[item];
+                 $('#artists').show();
+                $.ajax({
+                    url: 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=small&imgtype=face&q=' + artist.name,
+                    success: function (data) {
+                        $('#artist_photo').attr('src', data.responseData.results[0].url);
+                    },
+                    crossDomain: true,
+                    type: 'GET',
+                    dataType: 'jsonp'
+
+                });
+
+                $('#artist_name').text(artist.name);
+                $('#artist_style').text(artist.mbid);
+                var albums = {}
+                $.each(artist.tracks, function () {
+                    if (albums[this.release] == undefined) {
+                        albums[this.release] = this.release
+                    }
+                })
+                var als = ''
+                $.each(albums, function () {
+                    als = als + this;
+                    als = als + ' ';
+
+                });
+                $('#artists_album').text(als);
+                $('#artists_similaire').text(artist.similarity);
             }
+
 
         });
 
-    };
+    }
+        ;
 
-});
+})
+;
